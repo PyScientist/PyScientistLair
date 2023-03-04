ef80dc0a9fb650f2d1db512d3cc6b5a425e2aad9
PyScientist <dsa.msk@gmail.com>
2023-03-04 22:09:25 +0700

    -delete tracked main folder and add untracked file

---
+++ static/js/slides_stile.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('mousemove', () => {
+    clearActiveClasses()
+    slide.classList.add('active')
+  })
+}
+
+function clearActiveClasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
+}
